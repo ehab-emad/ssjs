1587af008604217727128a74e21666d0de050d2e
ehab-emad <ehabemad515@gmail.com>
2024-09-05 23:42:01 +0300

    fissrst commdlkkdiskksddddt

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
 const multer  = require('multer')
-const port = process.env.PORT || 30001;
+const port = process.env.PORT || 30009;
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'public/')
